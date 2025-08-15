63635c3e9a6e934317888168d101168f71f391f0
djzh23 <zouh.ijd@gmail.com>
2025-08-15 10:42:01 +0200

    First commit : bootstrap with Composer PSR-4 autoload and added .gitignore

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+session_start();
+require __DIR__ . '/../vendor/autoload.php';
+//  HttpKernel + Router 
---
+++ src/Core/Autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+final class Autoload
+{
+    public static function register(string $p, string $d): void
+    { /* Composer übernimmt */
+    }
+}
